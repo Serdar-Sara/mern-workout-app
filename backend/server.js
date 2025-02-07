901bda376e8f9fe85d352b4947ab728bcbe50262
Serdar Sara <serdarsara45@gmail.com>
2025-02-07 22:42:41 +0300

    Save changes before filter-branch

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,40 @@
-
-require('dotenv').config();
+require("dotenv").config();
 
 //The variable app typically represents an instance of an Express application. It is usually created using:
 const express = require("express");
 
-const workoutRoutes= require("./routes/workout");      // Import the router
+const mongoose = require("mongoose");
 
+const workoutRoutes = require("./routes/workout"); // Import the router
 
 //express app
 const app = express(); //express() initializes an Express application that we can use to define routes, middleware, and handle HTTP requests.
 
-
 //middleware
 
-app.use(express.json())
+app.use(express.json());
 
-app.use((req,res,next)=>{
-    console.log(req.path,req.method);
-    next();
+app.use((req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+});
 
-})
+// routes
+app.use("/api/workouts", workoutRoutes);
 
-
-app.use('/api/workouts' ,workoutRoutes);
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for request
+    app.listen(process.env.PORT, () => {
+      // gerçek port yerine process.env.PORT yazdım.   process.env is a built-in global object in Node.js that stores environment variables.
+      console.log("connected to db && listening on port ", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 ////routes e yolladım
 // //route
@@ -34,11 +46,6 @@ app.use('/api/workouts' ,workoutRoutes);
 // The Express server matches the request to the app.get('/', ...) route handler.
 // The callback function (req, res) => { ... } executes.
 // res.json() sends a JSON response { mssg: "welcome to my app " } to the client.
-
-//listen for request
-app.listen(process.env.PORT, () => {                  // 4000 yerine process.env.PORT yazdım.   process.env is a built-in global object in Node.js that stores environment variables.
-  console.log("listening on port ", process.env.PORT);
-});
 
 //The listen function takes a port number as its first argument.
 //The second argument to listen is a callback function that runs when the server starts successfully.
